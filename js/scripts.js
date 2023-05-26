a605e7daffafce5826f976507b262785bda6fb12
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-05-26 19:23:23 +0000

    feat: experimenting with objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,28 @@
 alert('Hello world');
 
-let favoriteFood = 'red curry'
-document.write(favoriteFood);
+let favoriteFood = 'red curry';
+let protein = 'tofu';
+let num = 2;
+document.write('My favorite food is ' + favoriteFood + ' but it\'s hard to make.');
+document.write(`My favorite food is ${favoriteFood} with ${num} ${protein}.`);
+
+document.write(2 + 2);
+document.write('2' + 2);
+document.write('2' + '2');
+
+
+let annesAge = 27;
+let anne = {
+  name: 'Anne',
+  age: annesAge,
+  child: {
+    name: 'Joe',
+    age: [2, 4, 5]
+  }
+};
+
+anne.name = 'Cool Anne';
+anne.child.age[0] = 10;
+
+console.log(anne.name);
+console.log(anne.child.age[0]);
