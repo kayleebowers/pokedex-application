b4fd134883b2ef053f2dccab96910a62496e4a9e
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-05-27 15:23:02 +0000

    feat: add id key to pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,21 @@ let pokemonList = [
   ivysaur = {
     name: "Ivysaur",
     height: 3.03,
-    types: ["grass", "poison"]
+    types: ["grass", "poison"],
+    id: 2
   },
 
   butterfree = {
     name: "Butterfree",
     height: 3.07,
-    types: ["bug", "flying"]
+    types: ["bug", "flying"],
+    id: 12
   },
 
   ninetales = {
     name: "Ninetales",
     height: 3.07,
-    types: ["fire"]
+    types: ["fire"],
+    id: 38
   }
 ];
