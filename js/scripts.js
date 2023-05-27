b1152038e2dbbe5268740cb846e8bf45189c9800
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-05-27 15:15:04 +0000

    feat: add butterfree object

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,16 @@ let ivysaur = {
   name: "Ivysaur",
   height: 3.03,
   types: ["grass", "poison"]
-}
+};
+
+let butterfree = {
+  name: "Butterfree",
+  height: 3.07,
+  types: ["bug", "flying"]
+};
+
+let ivysaur = {
+  name: "Ivysaur",
+  height: 3.03,
+  types: ["grass", "poison"]
+};
