b5ffbd2d23faba13d77b94575a7136e7938f2df1
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-05-27 15:17:07 +0000

    refactor: move pokemon objects into list array

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 //pokemon data
-let pokemonList = [];
+let pokemonList = [
+  ivysaur = {
+    name: "Ivysaur",
+    height: 3.03,
+    types: ["grass", "poison"]
+  },
 
-let ivysaur = {
-  name: "Ivysaur",
-  height: 3.03,
-  types: ["grass", "poison"]
-};
+  butterfree = {
+    name: "Butterfree",
+    height: 3.07,
+    types: ["bug", "flying"]
+  },
 
-let butterfree = {
-  name: "Butterfree",
-  height: 3.07,
-  types: ["bug", "flying"]
-};
-
-let ninetales = {
-  name: "Ninetales",
-  height: 3.07,
-  types: ["fire"]
-};
+  ninetales = {
+    name: "Ninetales",
+    height: 3.07,
+    types: ["fire"]
+  }
+];
