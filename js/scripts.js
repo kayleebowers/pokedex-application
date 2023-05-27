8d3976cad40cffa427b8b2dd02a0a39f645c59c7
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-05-27 15:15:28 +0000

    feat: add ninetales obj

--- js/scripts.js
+++ js/scripts.js
@@ -13,8 +13,8 @@ let butterfree = {
   types: ["bug", "flying"]
 };
 
-let ivysaur = {
-  name: "Ivysaur",
-  height: 3.03,
-  types: ["grass", "poison"]
+let ninetales = {
+  name: "Ninetales",
+  height: 3.07,
+  types: ["fire"]
 };
