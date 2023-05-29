c4747557a440c03e1b803da6c3d5de26ad2e428d
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-05-29 13:23:05 +0000

    bug: remove names from objects in pokemon arr

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,44 @@
 //pokemon data
 let pokemonList = [
-  (ivysaur = {
+  {
     name: "Ivysaur",
     height: 3.03,
     types: ["grass", "poison"],
     id: 2,
-  }),
+  },
 
-  (butterfree = {
+  {
     name: "Butterfree",
     height: 3.07,
     types: ["bug", "flying"],
     id: 12,
-  }),
+  },
 
-  (ninetales = {
+  {
     name: "Ninetales",
     height: 3.07,
     types: ["fire"],
     id: 38,
-  }),
+  },
 
-  (squirtle = {
+  {
     name: "Squirtle",
     height: 1.08,
     types: ["water"],
     id: 7,
-  }),
+  },
 
-  (raichu = {
+  {
     name: "Raichu",
     height: 2.07,
     types: ["electric"],
     id: 26,
-  }),
+  },
 
-  (jigglypuff = {
+  {
     name: "Jigglypuff",
     height: 1.08,
     types: ["normal", "fairy"],
     id: 39,
-  }),
+  },
 ];
