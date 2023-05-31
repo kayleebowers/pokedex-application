78d1eda00ba74d597a801477dc35235282b55a42
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-05-31 00:34:31 +0000

    refactor: change pokemonList arr to IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,49 @@
 //pokemon data
-let pokemonList = [
-  {
-    name: "Ivysaur",
-    height: 3.03,
-    types: ["grass", "poison"],
-    id: 2,
-  },
+let pokemonRepository = (function() {
+  [
+    {
+      name: "Ivysaur",
+      height: 3.03,
+      types: ["grass", "poison"],
+      id: 2,
+    },
 
-  {
-    name: "Butterfree",
-    height: 3.06,
-    types: ["bug", "flying"],
-    id: 12,
-  },
+    {
+      name: "Butterfree",
+      height: 3.06,
+      types: ["bug", "flying"],
+      id: 12,
+    },
 
-  {
-    name: "Ninetales",
-    height: 3.07,
-    types: ["fire"],
-    id: 38,
-  },
+    {
+      name: "Ninetales",
+      height: 3.07,
+      types: ["fire"],
+      id: 38,
+    },
 
-  {
-    name: "Squirtle",
-    height: 1.08,
-    types: ["water"],
-    id: 7,
-  },
+    {
+      name: "Squirtle",
+      height: 1.08,
+      types: ["water"],
+      id: 7,
+    },
 
-  {
-    name: "Raichu",
-    height: 2.07,
-    types: ["electric"],
-    id: 26,
-  },
+    {
+      name: "Raichu",
+      height: 2.07,
+      types: ["electric"],
+      id: 26,
+    },
 
-  {
-    name: "Jigglypuff",
-    height: 1.08,
-    types: ["normal", "fairy"],
-    id: 39,
-  },
-];
+    {
+      name: "Jigglypuff",
+      height: 1.08,
+      types: ["normal", "fairy"],
+      id: 39,
+    },
+  ];
+})();
 
 //write pokemon names and heights in document
 pokemonList.forEach(function(pokemon) {
